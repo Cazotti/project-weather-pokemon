7e73354470a2dc988ca027d07390ef3813358be9
Victor Anthony Mozer Cazotti <60163487+Cazotti@users.noreply.github.com>
2021-02-01 15:01:26 -0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const constants = {
+    openweathermap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        APPID: "525b109f4c26bbe21361ca653efa32b4"
+    },
+    pokeapi: {
+        TYPE_URL: "https://pokeapi.co/api/v2/type/",
+        STATS_URL: "https://pokeapi.co/api/v2/pokemon/",
+        IMAGE_URL: "https://pokeapi.co/api/v2/pokemon-form/"
+    }
+}
+
+module.exports = constants;
